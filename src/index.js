e1eaa8d69ee90040760215fd89a595fd86e288d2
Niks Kozlovs <kozlovs.niks1@gmail.com>
2022-05-23 15:21:58 +0300

    Set up file structure and reset the css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import './reset.scss';
 import './index.scss';
-import App from './App';
+import Home from './Route/Home';
+import Game from './Route/Game';
+import Instructions from './Route/Instructions';
+import AboutUs from './Route/AboutUs';
+import Header from './Component/Header';
+import Footer from './Component/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/game" element={<Game/>} />
+        <Route path="/instructions" element={<Instructions/>} />
+        <Route path="/about-us" element={<AboutUs/>} />
+      </Routes>
+    </Router>
+    <Footer />
   </React.StrictMode>
 );
