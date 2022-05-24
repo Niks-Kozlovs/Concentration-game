b4bb81ab29c1e5fc2cf7d9957222a23861dd59a0
Niks Kozlovs <kozlovs.niks1@gmail.com>
2022-05-25 00:34:43 +0300

    Added a very basic instructions page

--- src/Route/Instructions/index.jsx
+++ src/Route/Instructions/index.jsx
@@ -1,7 +1,14 @@
 function Instructions() {
     return (
-      <main>
-        Instructions
+      <main className="Instructions">
+        <h1>How to play!</h1>
+        <p>The rules are simple.
+          Tap on any 2 cards to flip them over.</p>
+          <ul>
+            <li>If they match, they get removed from the board</li>
+            <li>If they dont't match they get flipped back over</li>
+          </ul>
+          <p>The game ends when all the cards have been matched</p>
       </main>
     );
   }
